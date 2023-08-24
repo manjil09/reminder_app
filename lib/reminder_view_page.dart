65d76d290afcd7b9d086cdb2c905b613c82fd38f
manjil <manjilkoju09@gmail.com>
2023-08-24 17:27:28 +0545

    view page ui done

--- lib/reminder_view_page.dart
+++ lib/reminder_view_page.dart
@@ -8,6 +8,7 @@ class ReminderInterface extends StatefulWidget {
 }
 
 class _ReminderInterfaceState extends State<ReminderInterface> {
+  bool timeSelected = false;
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -27,12 +28,105 @@ class _ReminderInterfaceState extends State<ReminderInterface> {
                     bottomRight: Radius.circular(20),
                   ),
                 ),
-                child: const Text("reminder title"),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 32, horizontal: 12),
+                  child: Text(
+                    "reminder title",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
               ),
-              SwitchListTile(
-                title: const Text("Time"),
-                value: true,
-                onChanged: (bool value) {},
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    children: [
+                      SwitchListTile(
+                        title: const Text("Time"),
+                        value: timeSelected,
+                        onChanged: (bool value) {
+                          setState(() {
+                            timeSelected = value;
+                          });
+                        },
+                      ),
+                      Visibility(
+                        visible: timeSelected,
+                        maintainSize: false,
+                        child: const Column(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Icon(Icons.notifications),
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(12, 0, 0, 0),
+                                    child: Text("Date Time"),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Icon(Icons.loop),
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(12, 0, 0, 0),
+                                    child: Text("Don't repeat"),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Icon(Icons.volume_up),
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(12, 0, 0, 0),
+                                    child: Text("Medium"),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text(
+                            "Cancel",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          )),
+                      TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Save",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          )),
+                    ],
+                  ),
+                ),
               )
             ],
           ),
